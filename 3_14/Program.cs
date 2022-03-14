c16cf6b5f7fda2e8690fafeb97ea8cec2161e540
user <user@DESKTOP-6JBEF5K>
2022-03-14 11:25:24 +0800

    九九神功

--- 3_14/Program.cs
+++ 3_14/Program.cs
@@ -6,7 +6,23 @@ namespace _3_14
     {
         static void Main(string[] args)
         {
-            
+            int num = 0;
+           for(int i=1;i<=9;i++)
+            {
+                for(int j=1;j<=9;j++)
+                {
+                    num = i * j;
+                    Console.Write(i+"*"+j+"="+num);
+                    Console.Write(" ");
+
+                    if(num<10)//排版用
+                    { 
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
         }
     }
 }
