06f2bf6906ef20935b006ce16ef1d54857a64a9d
user <user@DESKTOP-6JBEF5K>
2022-03-14 11:15:19 +0800

    數星星

--- 3_14/Program.cs
+++ 3_14/Program.cs
@@ -6,7 +6,18 @@ namespace _3_14
     {
         static void Main(string[] args)
         {
-            
+            int num = 0;
+            Console.WriteLine("請輸入N:");
+            String input = Console.ReadLine();
+            num = int.Parse(input);
+            for (int i = 0; i < num; i++)
+            {
+                for (int i2 = 0; i2<= i; i2++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
